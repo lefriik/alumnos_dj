e61d4962fa1a26daa4e5e6c1028120a8adba3bd3
Emerio <emerio@emerio.com>
2022-02-11 21:26:31 -0300

    Modificacion Titulo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
         
         <h1 className="font-black text-5xl text-center text-white md:w-2/3 mx-auto">
             Seguimiento Alumnos 
-            <span className="text-yellow-300 "> Tutoriales</span>
+            <span className="text-yellow-300 "> Garage Session </span>
         </h1>
         
         
